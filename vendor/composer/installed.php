7c76f12e74a9a60ed5c0d79b02a182b00b2f9c19
Matthias Gutjahr <gutjahr@systopia.de>
2026-09-07 15:46:48 +0200

    Set version to 1.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.gdprx',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '31a0158eaeac683bd8cb6041e46f0bf96b4c564e',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.gdprx' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '31a0158eaeac683bd8cb6041e46f0bf96b4c564e',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
